122156d58f2389fc1350584415e36066e76fa720
Maic Lopez Saenz <maic@lopsae.com>
2015-12-03 13:26:53 -0800

    correct space indenting to tabs

--- abrupt/ViewController.swift
+++ abrupt/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  transitions
+//  abrupt
 //
 //  Created by Maic Lopez on 12/2/15.
 //  Copyright © 2015 Maic Lopez. All rights reserved.
@@ -10,40 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  var colorView: UIView!
+	var colorView: UIView!
 
 
-  init(color: UIColor) {
-    super.init(nibName: nil, bundle: nil)
-    colorView = UIView()
-    colorView.backgroundColor = color
-  }
+	init(color: UIColor) {
+		super.init(nibName: nil, bundle: nil)
+		colorView = UIView()
+		colorView.backgroundColor = color
+	}
 
-  required init?(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-  }
-
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.addSubview(colorView)
-  }
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 
 
-  override func viewWillAppear(animated: Bool) {
-    let margin: CGFloat = 2;
-    colorView.frame = CGRect(
-      x: view.bounds.origin.x + margin,
-      y: view.bounds.origin.x + margin,
-      width: view.bounds.size.width - margin*2,
-      height: view.bounds.size.height - margin*2)
-  }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.addSubview(colorView)
+	}
 
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+	override func viewWillAppear(animated: Bool) {
+		let margin: CGFloat = 2;
+		colorView.frame = CGRect(
+			x: view.bounds.origin.x + margin,
+			y: view.bounds.origin.x + margin,
+			width: view.bounds.size.width - margin*2,
+			height: view.bounds.size.height - margin*2)
+	}
+
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 
 }
